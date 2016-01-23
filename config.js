ce99b3591e0ed1874ef23889ed6abd5115e8f5bc
lidianhao123 <dainli@outlook.com>
2016-01-23 22:26:09 +0800

    add config.js

--- config.js
+++ config.js
@@ -1,88 +1,88 @@
 /**
- - * config
- - */
- -
- -var path = require('path');
- -
- -var config = {
- -  // debug 为 true 时，用于本地调试
- -  debug: true,
- -
- -  get mini_assets() { return !this.debug; }, // 是否启用静态文件的合并压缩，详见视图中的Loader
- -
- -  name: '', // 社区名字
- -  description: '', // 社区的描述
- -  keywords: '',
- -
- -  // 添加到 html head 中的信息
- -  site_headers: [
- -    '<meta name="author" content="EDP@TAOBAO" />'
- -  ],
- -  site_logo: '/public/images/cnodejs_light.svg', // default is `name`
- -  site_icon: '/public/images/cnode_icon_32.png', // 默认没有 favicon, 这里填写网址
- -  // 右上角的导航区
- -  site_navs: [
- -    // 格式 [ path, title, [target=''] ]
- -    [ '/about', '关于' ]
- -  ],
- -  // cdn host，如 http://cnodejs.qiniudn.com
- -  site_static_host: '', // 静态文件存储域名
- -  // 社区的域名
- -  host: '',
- -  // 默认的Google tracker ID，自有站点请修改，申请地址：http://www.google.com/analytics/
- -  google_tracker_id: '',
- -  // 默认的cnzz tracker ID，自有站点请修改
- -  cnzz_tracker_id: '',
- -
- -  // mongodb 配置
- -  db: 'mongodb://127.0.0.1/blog',
- -  dbuser: '',
- -  dbpass: '',
- -
- -  // redis 配置，默认是本地
- -  redis_host: '127.0.0.1',
- -  redis_port: 6379,
- -  redis_db: 0,
- -
- -  session_secret: '', // 务必修改
- -  auth_cookie_name: '',
- -
- -  // 程序运行的端口
- -  port: 3300,
- -
- -  // 文章列表显示的话题数量
- -  list_topic_count: 10,
- -
- -  // admin 可删除话题，编辑标签，设某人为达人
- -  admins: { user_login_name: true },
- -
- -  // 是否允许直接注册（否则只能走 github 的方式）
- -  allow_sign_up: true,
- -
- -  // newrelic 是个用来监控网站性能的服务
- -  newrelic_key: 'yourkey',
- -
- -  // 文件上传配置
- -  // 注：如果填写 qn_access，则会上传到 7牛，以下配置无效
- -  upload: {
- -    path: path.join(__dirname, 'public/upload/'),
- -    url: '/upload/'
- -  },
- -
- -  // 版块
- -  tabs: [
- -    ['share', '分享'],
- -    ['ask', '问答'],
- -    ['job', '招聘'],
- -  ],
- -
- -  create_post_per_day: 1000, // 每个用户一天可以发的主题数
- -  create_reply_per_day: 1000, // 每个用户一天可以发的评论数
- -  visit_per_day: 1000, // 每个 ip 每天能访问的次数
- -};
- -
- -if (process.env.NODE_ENV === 'test') {
- -  config.db = 'mongodb://127.0.0.1/blog_test';
- -}
- -
- -module.exports = config;
+ * config
+ */
+
+var path = require('path');
+
+var config = {
+  // debug 为 true 时，用于本地调试
+  debug: true,
+
+  get mini_assets() { return !this.debug; }, // 是否启用静态文件的合并压缩，详见视图中的Loader
+
+  name: '全栈开发', // 社区名字
+  description: '个人全栈开发博客，记录前端工程师全栈开发过程中的点滴', // 社区的描述
+  keywords: 'nodejs, node, express',
+
+  // 添加到 html head 中的信息
+  site_headers: [
+    '<meta name="author" content="EDP@TAOBAO" />'
+  ],
+  site_logo: '/public/images/cnodejs_light.svg', // default is `name`
+  site_icon: '/public/images/cnode_icon_32.png', // 默认没有 favicon, 这里填写网址
+  // 右上角的导航区
+  site_navs: [
+    // 格式 [ path, title, [target=''] ]
+    [ '/about', '关于' ]
+  ],
+  // cdn host，如 http://cnodejs.qiniudn.com
+  site_static_host: '', // 静态文件存储域名
+  // 社区的域名
+  host: 'www.dainli.com',
+  // 默认的Google tracker ID，自有站点请修改，申请地址：http://www.google.com/analytics/
+  google_tracker_id: '',
+  // 默认的cnzz tracker ID，自有站点请修改
+  cnzz_tracker_id: '',
+
+  // mongodb 配置
+  db: 'mongodb://127.0.0.1/blog',
+  dbuser: '',
+  dbpass: '',
+
+  // redis 配置，默认是本地
+  redis_host: '127.0.0.1',
+  redis_port: ,
+  redis_db: 0,
+
+  session_secret: '', // 务必修改
+  auth_cookie_name: '',
+
+  // 程序运行的端口
+  port: 3000,
+
+  // 文章列表显示的话题数量
+  list_topic_count: 10,
+
+  // admin 可删除话题，编辑标签，设某人为达人
+  admins: { user_login_name: true },
+
+  // 是否允许直接注册（否则只能走 github 的方式）
+  allow_sign_up: true,
+
+  // newrelic 是个用来监控网站性能的服务
+  newrelic_key: 'yourkey',
+
+  // 文件上传配置
+  // 注：如果填写 qn_access，则会上传到 7牛，以下配置无效
+  upload: {
+    path: path.join(__dirname, 'public/upload/'),
+    url: '/upload/'
+  },
+
+  // 版块
+  tabs: [
+    ['share', '分享'],
+    ['ask', '问答'],
+    ['job', '招聘'],
+  ],
+
+  create_post_per_day: 1000, // 每个用户一天可以发的主题数
+  create_reply_per_day: 1000, // 每个用户一天可以发的评论数
+  visit_per_day: 1000, // 每个 ip 每天能访问的次数
+};
+
+if (process.env.NODE_ENV === 'test') {
+  config.db = 'mongodb://127.0.0.1/blog_test';
+}
+
+module.exports = config;
