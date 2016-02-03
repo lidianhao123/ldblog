91ee90f885bd215490021f7e92e91aa4b5a131d2
lidianhao123 <dainli@outlook.com>
2016-02-03 23:07:35 +0800

    modify

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -68,7 +68,7 @@ exports.authUser = function (req, res, next) {
 exports.userRequired = function (req, res, next) {
   if (!req.session || !req.session.user) {
     // return res.status(403).send('forbidden!');
-    res.redirect('/users/login')
+    return res.redirect('/users/login')
   }
 
   next();
