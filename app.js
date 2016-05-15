a6944a9420533433d9b34ab6917d8c3ea77b20cd
lidianhao123 <dainli@outlook.com>
2016-05-15 21:29:54 +0800

    modify

--- app.js
+++ app.js
@@ -55,7 +55,20 @@ app.use(busboy({
   }
 }));
 
-app.use(cors());
+// app.use(cors());
+//allow custom header and CORS
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); /*让options请求快速返回*/
+  }
+  else {
+    next();
+  }
+});
 
 app.use('/', routes);
 app.use('/users', users);
